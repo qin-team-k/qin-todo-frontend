12d71172fd567ce374a3ea44ce5337d79b629770
mu0363 <dde036@gmail.com>
2022-03-06 23:50:31 +0900

    feat: create profile page

---
+++ src/pages/profile.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import { Header } from "@/components/Header";
+
+const Login: NextPage = () => {
+  return (
+    <div>
+      <Header />
+      <h2>profile</h2>
+    </div>
+  );
+};
+
+export default Login;

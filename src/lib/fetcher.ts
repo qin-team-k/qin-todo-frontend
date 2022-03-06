974c4abdd5a4a67ecab9828f0f75b58e5ab9371b
mu0363 <dde036@gmail.com>
2022-03-06 23:49:19 +0900

    fix: add withCredential in axios

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,2 +1,3 @@
 import axios from "axios";
-export const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+export const fetcher = (url: string) =>
+  axios.get(url, { withCredentials: true }).then((res) => res.data);

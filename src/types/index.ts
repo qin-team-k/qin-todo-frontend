24144b17ac6a996362d20a71d85955783e2088b9
mu0363 <dde036@gmail.com>
2022-03-06 23:50:54 +0900

    feat: add User type

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,12 @@ export type Todo = {
   updatedAt: Date;
   userId: string;
 };
+
+export type User = {
+  id: string;
+  avatarUrl: string;
+  createdAt: Date;
+  email: string;
+  updatedAt: Date;
+  username: string;
+};
